954fd3d4d50a900433b5da73bb540aa801e29ff3
gittihubs <61996106+gittihubs@users.noreply.github.com>
2020-06-29 20:29:47 -0700

    Add files via upload
    Update

---
+++ script.js
@@ -0,0 +1,87 @@
+const database = [
+        {
+          email: "stephen.stanley.chen@gmail.com",
+          username: "Stanley",
+          password: "toni0217"
+        },
+        {
+          email: "DB_legends@gmail.com",
+          username: "Son_Goku",
+          password: "DB_g0ds"
+        },
+        {
+          email: "abcya@gmail.com",
+          username: "ABCya",
+          password: "ahbseeya"
+        },
+        {
+          email: "NBA_live@gmail.com",
+          username: "NBA_global",
+          password: "ThisIsWhyWePlay"
+        },
+        {
+          email: "Brawlstars@gmail.com",
+          username: "BS_global",
+          password: "Brawlday"
+        },
+        {
+          email: "FBnews@gmail.com",
+          username: "FB_News",
+          password: "News_FakeB"
+        }
+];
+
+const newsFeed = [
+{
+    username: "ABCya",
+    Post: "Go to ABCYA to play all the traash games."
+},
+{
+    username: "NBA_global",
+    Post: "Watch live games at your fingertips!"
+
+},
+{
+  username: "Stanley",
+  Post: "Nothing new today."
+},
+{
+  username: "Son_Goku",
+  Post: "DBL sucks because of all the favoring they give to the people that buy."
+},
+{
+  username: "BS_global",
+  Post: "The new brawler Nani is out and the season ends in 22 days!"
+},
+{
+  username: "FB_News",
+  Post: "ABCYA and Son_Goku's account have been hacked by an unkown, we will keep invesigating on this problem"
+}
+];
+
+ /* const userValid = (email, username, password) => {
+  for (var i=0; i < database.length; i++){
+    if(database[i].email === email &&
+       database[i].username === username &&
+        database[i].password === password) {
+          return true;
+        } 
+    }
+    return false;
+}
+
+const signIn = (email, username, password) => {  
+  if (userValid(email, username, password)) {
+          alert("You are now signed in.");
+          console.log(newsFeed);
+        } else {
+            alert("Your email or username or password is incorrect.");
+        }
+  }
+
+let emailPrompt = prompt("Please enter your email.");
+let userNamePrompt = prompt("Please enter your username.");
+let passWordPrompt = prompt("Please enter your password.");
+
+signIn(emailPrompt, userNamePrompt, passWordPrompt); 
+ */
